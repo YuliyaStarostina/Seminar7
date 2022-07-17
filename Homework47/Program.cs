8891e5d7d6d4f4a9fd7da19e5d75e4c433be7a8f
Starostina <pankovays@mail.ru>
2022-07-17 17:04:30 +0700

    Домашняя работа к семинару 7

---
+++ Homework47/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+double[,] myArr = new double[3, 4]; 
+Random random = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        myArr[i, j] = random.NextDouble()*100;
+        System.Console.Write("{0, 6:F2}", myArr[i, j]);
+    }
+    System.Console.WriteLine();
+}
