3d72cd6bb4e93374d81ccbd595a7c9f30a4b3240
Starostina <pankovays@mail.ru>
2022-07-17 16:41:22 +0700

    Задача 46 семинар 7

---
+++ Zadacha46/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте двумерный массив размером M x N, заполненный случайными целыми числами.
+// M=3, N=4
+int[,] myArr = new int [3, 4];
+Random ran = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        myArr[i, j] = ran.Next(1, 100);
+        System.Console.Write("{0}\t", myArr[i, j]);
+    }
+    System.Console.WriteLine();
+}
+
