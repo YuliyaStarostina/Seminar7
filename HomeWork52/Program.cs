fce5ef5c9d577b13dc5d947781c43337b161298f
Starostina <pankovays@mail.ru>
2022-07-20 17:34:18 +0700

    Задача 52 к ДЗ семинара 7

---
+++ HomeWork52/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+int[,] mtrx = new int[4, 6];
+ var ran = new Random();
+{
+        for (int i = 0; i < mtrx.GetLength(0); i++)
+        {
+                for (int j = 0; j < mtrx.GetLength(1); j++)
+                {
+                     mtrx[i, j] = ran.Next(0, 10);
+                     System.Console.Write($"{mtrx[i, j], 2}");   
+                }
+                System.Console.WriteLine();
+        }
+    
+}
+AverageArithmetical(mtrx);
+void AverageArithmetical(int[,] array)
+{
+    double mathAverage = 0;
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum += array[j, i];
+        }
+        mathAverage = sum / array.GetLength(0);
+        Console.WriteLine($"Среднее арифметическое элементов в столбце: {Math.Round(mathAverage, 2)}");
+    }
+}
